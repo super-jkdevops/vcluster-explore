e6fb4f54c8abe4da6f5530101f8a556c6de88ae1
Janusz Kujawa <janusz.kujawa@gmail.com>
2022-02-25 15:01:22 +0100

    ArgoCD vcluster deployment

--- bin/argocd.sh
+++ bin/argocd.sh
@@ -12,15 +12,6 @@ while [ "$(kubectl get pods -n kube-system -l k8s-app=kube-dns -o jsonpath='{.it
   echo "Waiting core pods to be running"
 done
 
-echo "!!! ArgoCD installation !!!"
-echo "Helm repository adding"
-helm repo add argo https://argoproj.github.io/argo-helm
-helm repo update
-#helm install argocd argo/argo-cd -n argocd --create-namespace --set server.service.type="LoadBalancer" --set server.service.servicePortHttp="8080" --set server.service.servicePortHttps="8443" 
-
-#echo "Wait... ArgoCD installation inprogress!"
-#kubectl wait --timeout=500s -n argocd --for=condition=ready pod -l app.kubernetes.io/name=argocd-server
-
 echo "Dump ArgoCD initial password"
 kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d > /var/tmp/$CLUSTER/argocd/ArgoCD-initial-pass.txt
 chmod 0600 /var/tmp/$CLUSTER/argocd/ArgoCD-initial-pass.txt
@@ -32,10 +23,7 @@ PASS=$(cat /var/tmp/$CLUSTER/argocd/ArgoCD-initial-pass.txt)
 USER="admin"
 
 echo "ArgoCD command line logging"
-argocd login --insecure $SERVER:$PORT --username=$USER --password=$PASS
-
-echo "ArgoCD command line configuration"
-argocd proj create vcluster-gitops --dest="*,*" --src="*" --allow-cluster-resource="*/*" --description="vCluster GitOps Platform"
+argocd login  $SERVER:$PORT --username=$USER --password=$PASS --insecure
 
 echo "ArgoCD list resources"
 echo ""
